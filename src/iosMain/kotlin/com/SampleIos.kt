4a45e8e2ab25e1e309fd8cf2b006c055b67da9fb
dmitrijmihajluk <mikhajlyuk@gmail.com>
2019-05-25 14:39:53 +0300

    init

---
+++ src/iosMain/kotlin/com/SampleIos.kt
@@ -0,0 +1,9 @@
+package com
+
+actual class Sample {
+    actual fun checkMe() = 7
+}
+
+actual object Platform {
+    actual fun name(): String = "iOS"
+}
---
+++ src/iosTest/kotlin/com/SampleTestsNative.kt
@@ -0,0 +1,11 @@
+package com
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class SampleTestsNative {
+    @Test
+    fun testHello() {
+        assertTrue("iOS" in hello())
+    }
+}
